7e4b6fd74a7e5a2c80fa36672e8d8975821f5a8d
ilshat kushmanbetov <ilshat26101993@list.ru>
2025-03-20 14:51:56 +0600

    feat(refactor): Добавление проекту структуры

--- src/main.py
+++ src/main.py
@@ -5,9 +5,9 @@ import lightgbm as lgb
 
 # Загрузка данных
 def load_data():
-    id1 = pd.read_csv('../id_597.csv')
-    id2 = pd.read_csv('../id_16.csv')
-    id3 = pd.read_csv('../id_34.csv')
+    id1 = pd.read_csv('../data/raw/id_597.csv')
+    id2 = pd.read_csv('../data/raw/id_16.csv')
+    id3 = pd.read_csv('../data/raw/id_34.csv')
     return pd.concat([id1, id2, id3])
 
 
@@ -32,6 +32,11 @@ def train_model(X, y):
     model = lgb.LGBMRegressor()
     model.fit(X_train, y_train)
     return model
+
+
+def make_predictions(model, tomorrow_date):
+    # Логика прогнозирования
+    pass
 
 
 if __name__ == "__main__":
